4bdd9cc0c715b0fc2614e0231948a24ad78af080
rib-president <fnslfnslfnslfnslfnsl@gmail.com>
2021-12-21 09:06:54 +0900

    upload 20211221

--- JavaSpace/Java_Practice2/src/practice_1/Question12.java
+++ JavaSpace/Java_Practice2/src/practice_1/Question12.java
@@ -3,17 +3,27 @@ package practice_1;
 // 1~10000사이에 8이 몇번 나오는가?
 
 public class Question12 {
-	static int count = 0;
+//	static int count = 0;
 	public static void main(String[] args) {
-
+		int count = 0;
 		for(int i=1;i<=10000;i++) {
-			countEight(i);
+			int tmp = i;
+			while(tmp != 0) {
+				if(tmp % 10 == 8)
+					count++;
+				tmp /= 10;
+			}
 		}
 		System.out.println(count);
+		
+//		for(int i=1;i<=10000;i++) {
+//		countEight(i);
+//		}
+//		System.out.println(count);
 	}
 	
-	public static void countEight(int num) {
-		if(num % 10 == 8) count++;
-		if(num > 10) countEight(num / 10);
-	}
+//	public static void countEight(int num) {
+//		if(num % 10 == 8) count++;
+//		if(num > 10) countEight(num / 10);
+//	}
 }
