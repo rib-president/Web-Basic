bfbb928ac01029f1b4990bcbedb56ea8bdd59f2a
rib-president <fnslfnslfnslfnslfnsl@gmail.com>
2023-06-01 00:55:04 +0900

    react blog-frontend

--- ReactSpace/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ ReactSpace/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -4,13 +4,24 @@ import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
 	const { id } = ctx.params;
 	if (!ObjectId.isValid(id)) {
 		ctx.status = 400; // Bad Request;
 		return;
 	}
-	return next();
+	try {
+		const post = await Post.findById(id);
+		// 포스트가 존재하지 않을 때
+		if (!post) {
+			ctx.status = 404; // Not Found
+			return;
+		}
+		ctx.state.post = post;
+		return next();
+	} catch (e) {
+		ctx.throw(500, e);
+	}
 };
 
 /*
@@ -42,6 +53,7 @@ export const write = async (ctx) => {
 		title,
 		body,
 		tags,
+		user: ctx.state.user,
 	});
 	try {
 		await post.save();
@@ -61,14 +73,21 @@ export const list = async (ctx) => {
 		return;
 	}
 
+	const { tag, username } = ctx.query;
+	// tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 안 넣음
+	const query = {
+		...(username ? { 'user.username': username } : {}),
+		...(tag ? { tags: tag } : {}),
+	};
+
 	try {
-		const posts = await Post.find()
+		const posts = await Post.find(query)
 			.sort({ _id: -1 })
 			.limit(10)
 			.skip((page - 1) * 10)
 			.lean()
 			.exec();
-		const postCount = await Post.countDocuments().exec();
+		const postCount = await Post.countDocuments(query).exec();
 		ctx.set('Last-Page', Math.ceil(postCount / 10));
 		ctx.body = posts.map((post) => ({
 			...post,
@@ -81,17 +100,7 @@ export const list = async (ctx) => {
 };
 
 export const read = async (ctx) => {
-	const { id } = ctx.params;
-	try {
-		const post = await Post.findById(id).exec();
-		if (!post) {
-			ctx.status = 404; // Not Found
-			return;
-		}
-		ctx.body = post;
-	} catch (e) {
-		ctx.throw(500, e);
-	}
+	ctx.body = ctx.state.post;
 };
 
 export const remove = async (ctx) => {
@@ -142,4 +151,13 @@ export const update = async (ctx) => {
 	} catch (e) {
 		ctx.throw(500, e);
 	}
+};
+
+export const checkOwnPost = (ctx, next) => {
+	const { user, post } = ctx.state;
+	if (post.user._id.toString() !== user._id) {
+		ctx.status = 403;
+		return;
+	}
+	return next();
 };
